fc181d6bf15c6a03d719506199f908dd9664ab01
mikka <mikkanthrope@gmail.com>
2018-04-16 16:43:37 -0700

    Create application server and React frontend app

---
+++ example-frontend/src/container/Main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import axios from 'axios';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {ponged: 'Not Ponged'}
+
+    this.ping = this.ping.bind(this);
+  }
+
+  ping() {
+    axios.get("http://localhost:8080/pong")
+      .then(res => {
+        alert("Received Successful response from server!");
+        this.setState({ponged: 'Ponged! '});
+      }, err => {
+        alert("Server rejected response with: " + err);
+    });
+  }
+
+  render() {
+    return (
+      <div className="Main">
+        <header className="App-header">
+          <h1 className="App-title">Ping</h1>
+        </header>
+        <div className="App-intro">
+          <div>
+            <button onClick={this.ping}>Ping!</button>
+            <div>Ponged: {this.state.ponged}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
